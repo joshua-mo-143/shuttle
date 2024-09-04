fce30d84bd449eff8a14a32db9005edc228fd564
Joshua Mo <joshua.mo.876@gmail.com>
2024-09-04 01:17:50 +0100

    refactor: add comment to arg

--- cargo-shuttle/src/args.rs
+++ cargo-shuttle/src/args.rs
@@ -143,6 +143,7 @@ pub enum Command {
 
 #[derive(Parser, Default)]
 pub struct ExplainArgs {
+    /// Sends your file contents from captured errors to the LLM.
     #[arg(short, long, default_value_t = false)]
     pub send_files: bool,
 }
